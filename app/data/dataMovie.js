e20e75878507759c8a3394c13cd8c1be5c5f87aa
Parot Enzo <parot23@mmi-dev.unilim.fr>
2025-04-08 10:46:29 +0200

    la bar noir est revenue

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -1,7 +1,10 @@
+
 let HOST_URL = "https://mmi.unilim.fr/~parot23/SAE2.03-Parot";//"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
 
 let DataMovie = {};
 
+
+/*
  /**
      * Fetches data from the server based on the specified day.
      *
@@ -11,8 +14,9 @@ let DataMovie = {};
      * DataMenu.request permet de récupérer des données depuis le serveur.
      * Elle prend en paramètre un jour (lundi mardi...) de la semaine et 
      * renvoie les données contenues dans la réponse du serveur (data).
-     */
-DataMenu.requestMovies = async function(day){
+ 
+ *//*
+ DataMovie.requestMovies = async function(day){
     // fetch permet d'envoyer une requête HTTP à l'URL spécifiée. 
     // L'URL est construite en concaténant HOST_URL à "/server/script.php?direction=" et la valeur de la variable dir. 
     // L'URL finale dépend de la valeur de HOST_URL et de dir.
@@ -20,10 +24,15 @@ DataMenu.requestMovies = async function(day){
     // answer est la réponse du serveur à la requête fetch.
     // On utilise ensuite la méthode json() pour extraire de cette réponse les données au format JSON.
     // Ces données (data) sont automatiquement converties en objet JavaScript.
-    let data = await answer.json();
+    let movies = await answer.json();
     // Enfin, on retourne ces données.
-    return data;
-}
+    return movies;
+}*/
 
+DataMovie.requestMovies = async function () {
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=getMovie");
+    let movies = await answer.json();
+    return movies;
+  };
 
 export {DataMovie};
