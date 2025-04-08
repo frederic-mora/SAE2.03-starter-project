3c314aa36fa82c34141ad9e7268f14ee29ff2134
Parot Enzo <parot23@mmi-dev.unilim.fr>
2025-04-08 15:31:26 +0200

    je sais plus

--- server/controller.php
+++ server/controller.php
@@ -24,3 +24,30 @@ function readMoviesController(){
     $movies = getAllMovies();
     return $movies;
 }
+
+function addController(){
+   
+    $titre = $_REQUEST['titre'] ?? null;
+    $realisateur = $_REQUEST['realisateur'] ?? null;
+    $annee = $_REQUEST['annee'] ?? null;
+    $duree = $_REQUEST['duree'] ?? null;
+    $desc = $_REQUEST['desc'] ?? null;
+    $categorie = $_REQUEST['categorie'] ?? null;
+    $image = $_REQUEST['image'] ?? null;
+    $url = $_REQUEST['url'] ?? null;
+    $age = $_REQUEST['age'] ?? null;
+
+    
+    if (empty($titre) || empty($realisateur) || empty($annee) || empty($duree) || empty($desc) || empty($categorie) || empty($image) || empty($url) || empty($age)) {
+        return "Erreur : Tous les champs doivent être remplis.";
+    }
+    
+    $ok = addMovie($titre, $realisateur, $annee, $duree, $desc, $categorie, $image, $url, $age);
+    
+    if ($ok!=0){
+      return "Le film $titre a été ajouté avec succès !";
+    } 
+    else{
+      return "Erreur lors de l'ajout du film $titre !";
+    }
+  }
